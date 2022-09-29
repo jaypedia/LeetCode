cb9d8c5d171731eed1987c08edf1c656d59e020a
Millie <85419343+jaypedia@users.noreply.github.com>
2022-09-29 16:30:06 +0900

    Time: 106 ms (63.72%), Space: 45 MB (59.50%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -11,13 +11,14 @@
  * @return {boolean}
  */
 var hasCycle = function (head) {
-  const set = new Set();
-  while (head) {
-    set.add(head);
-    head = head.next;
-    if (!head) return false;
-    if (set.has(head)) return true;
+  let slow = head;
+  let fast = head;
+  while(fast && fast.next) {
+    
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
 
   return false;
-};
+}
